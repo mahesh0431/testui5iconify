168c1e589db27fda6e1500f7cd3e163c4f71e908
Mahesh Kumar Palavalli <mahesh.p@EGA.LAN>
2020-08-12 17:48:41 +0400

    mahesh changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,80 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+  } from "vscode-languageclient";
+
+  import {
+	SERVER_PATH,
+	ServerInitializationOptions,
+  } from "@ui5-language-assistant/language-server";
+
+  let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) : Promise<void> {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "xmltoolstest" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('xmltoolstest.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const channel = vscode.window.createOutputChannel("UI5Iconify");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from xmltoolstest!');
-	});
+	const initializationOptions: ServerInitializationOptions = {
+		modelCachePath: context.globalStoragePath,
+	  };
 
-	context.subscriptions.push(disposable);
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: "file", pattern: "**/*.{view,fragment}.xml" }],
+		synchronize: {
+		  fileEvents: [vscode.workspace.createFileSystemWatcher("**/manifest.json")],
+		},
+		// Sending a channel we created instead of only giving it a name in outputChannelName so that if necessary we
+		// can print to it before the client starts (in this method)
+		outputChannel: channel,
+		//initializationOptions: initializationOptions,
+	  };
+	  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+	  const serverOptions: ServerOptions = {
+		run: { module: './langu.ts', transport: TransportKind.ipc },
+		debug: {
+		  module: './langu.ts',
+		  transport: TransportKind.ipc,
+		  options: debugOptions,
+		},
+	  };
+	  
+	  client = new LanguageClient(
+		"UI5Iconify",
+		"UI5Iconify",
+		serverOptions,
+		clientOptions
+	  );
+	
+	  client.start();
+
+	// // The command has been defined in the package.json file
+	// // Now provide the implementation of the command with registerCommand
+	// // The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('xmltoolstest.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from xmltoolstest!');
+	// });
+
+	//context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+	  return undefined;
+	}
+	return client.stop();
+  }
---
+++ src/langu.ts
@@ -0,0 +1,250 @@
+import { forEach } from "lodash";
+import {
+  createConnection,
+  TextDocuments,
+  TextDocumentSyncKind,
+  ProposedFeatures,
+  TextDocumentPositionParams,
+  CompletionItem,
+  InitializeParams,
+  Hover,
+  DidChangeConfigurationNotification,
+} from "vscode-languageserver";
+import { URI } from "vscode-uri";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import { ftruncate } from "fs";
+// import { UI5SemanticModel } from "@ui5-language-assistant/semantic-model-types";
+// import {
+//   clearSettings,
+//   setGlobalSettings,
+//   clearDocumentSettings,
+//   setSettingsForDocument,
+//   hasSettingsForDocument,
+//   getSettingsForDocument,
+// } from "@ui5-language-assistant/settings";
+// import { ServerInitializationOptions } from "../api";
+// import { getSemanticModel } from "./ui5-model";
+// import { getCompletionItems } from "./completion-items";
+// import { getXMLViewDiagnostics } from "./xml-view-diagnostics";
+// import { getHoverResponse } from "./hover";
+// import {
+//   getFlexEnabledFlagForXMLFile,
+//   isManifestDoc,
+//   initializeManifestData,
+//   updateManifestData,
+// } from "./manifest-handling";
+// import {
+//   diagnosticToCodeActionFix,
+//   QUICK_FIX_STABLE_ID_COMMAND,
+//   QUICK_FIX_FILE_STABLE_ID_COMMAND,
+// } from "./quick-fix";
+// import { executeCommand } from "./commads";
+
+const connection = createConnection(ProposedFeatures.all);
+const documents = new TextDocuments(TextDocument);
+// let semanticModelLoaded: Promise<UI5SemanticModel> | undefined = undefined;
+let manifestStateInitialized: Promise<void[]> | undefined = undefined;
+// let initializationOptions: ServerInitializationOptions | undefined;
+let hasConfigurationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+  const capabilities = params.capabilities;
+  const workspaceFolderUri = params.rootUri;
+  if (workspaceFolderUri !== null) {
+    const workspaceFolderAbsPath = URI.parse(workspaceFolderUri).fsPath;
+    // manifestStateInitialized = initializeManifestData(workspaceFolderAbsPath);
+  }
+
+  // Does the client support the `workspace/configuration` request?
+  // If not, we will fall back using global settings
+  hasConfigurationCapability =
+    capabilities.workspace !== undefined &&
+    (capabilities.workspace.configuration ?? false);
+
+  // These options are passed from the client extension in clientOptions.initializationOptions
+//   initializationOptions = params.initializationOptions;
+  return {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Full,
+      completionProvider: {
+        resolveProvider: true,
+        // TODO: can the trigger characters be more contextual?
+        //       e.g: "<" of open tag only, not else where
+        triggerCharacters: ['"', "'", ":", "<"],
+      },
+      hoverProvider: true,
+      codeActionProvider: true,
+      // Each command executes a different code action scenario
+      executeCommandProvider: {
+        commands: [
+        //   QUICK_FIX_STABLE_ID_COMMAND,
+        //   QUICK_FIX_FILE_STABLE_ID_COMMAND,
+        ],
+      },
+    },
+  };
+});
+
+connection.onInitialized(async () => {
+//   semanticModelLoaded = getSemanticModel(initializationOptions?.modelCachePath);
+
+  if (hasConfigurationCapability) {
+    // Register for all configuration changes
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+});
+
+connection.onCompletion(
+  async (
+    textDocumentPosition: TextDocumentPositionParams
+  ): Promise<CompletionItem[]> => {
+      return new Promise(function(resolve){
+          resolve();
+      });
+    // if (semanticModelLoaded !== undefined) {
+    //   const model = await semanticModelLoaded;
+    //   const documentUri = textDocumentPosition.textDocument.uri;
+    //   const document = documents.get(documentUri);
+    //   if (document) {
+    //     ensureDocumentSettingsUpdated(document.uri);
+    //     const x1 = await ft1("1231");
+    //     const documentSettings = await getSettingsForDocument(document.uri);
+    //     return getCompletionItems({
+    //       model,
+    //       textDocumentPosition,
+    //       document,
+    //       documentSettings,
+    //     });
+    //   }
+    // }
+    // return [];
+  }
+);
+
+connection.onCompletionResolve(
+  (item: CompletionItem): CompletionItem => {
+    return item;
+  }
+);
+
+connection.onHover(
+  async (
+    textDocumentPosition: TextDocumentPositionParams
+  ): Promise<Hover | undefined> => {
+    return new Promise(function(resolve){
+        resolve();
+    });
+  }
+);
+
+connection.onDidChangeWatchedFiles(async (changeEvent) => {
+//   forEach(changeEvent.changes, async (change) => {
+//     const uri = change.uri;
+//     if (!isManifestDoc(uri)) {
+//       return;
+//     }
+
+//     await updateManifestData(uri, change.type);
+//   });
+});
+
+documents.onDidChangeContent(async (changeEvent) => {
+  if (
+    // semanticModelLoaded === undefined ||
+    manifestStateInitialized === undefined ||
+    !isXMLView(changeEvent.document.uri)
+  ) {
+    return;
+  }
+
+//   const ui5Model = await semanticModelLoaded;
+  await manifestStateInitialized;
+  const documentUri = changeEvent.document.uri;
+  const document = documents.get(documentUri);
+  if (document !== undefined) {
+    const documentPath = URI.parse(documentUri).fsPath;
+    // const flexEnabled = getFlexEnabledFlagForXMLFile(documentPath);
+    const diagnostics = getXMLViewDiagnostics({
+      document,
+      ui5Model,
+      flexEnabled,
+    });
+    connection.sendDiagnostics({ uri: changeEvent.document.uri, diagnostics });
+  }
+});
+
+connection.onCodeAction(async (params) => {
+  if (semanticModelLoaded === undefined) {
+    return;
+  }
+
+  const ui5Model = await semanticModelLoaded;
+  const docUri = params.textDocument.uri;
+  const textDocument = documents.get(docUri);
+  if (textDocument === undefined) {
+    return undefined;
+  }
+
+  const diagnostics = params.context.diagnostics;
+  const codeActions = diagnosticToCodeActionFix(
+    textDocument,
+    diagnostics,
+    ui5Model
+  );
+  return codeActions;
+});
+
+connection.onExecuteCommand(async (params) => {
+  executeCommand(connection, params);
+});
+
+function ensureDocumentSettingsUpdated(resource: string): void {
+  // There are 2 flows for settings, depending on the client capabilities:
+  // 1. The client doesn't support workspace/document-level settings (workspace/configuration request).
+  //    In this case we use global settings (which arrive in onDidChangeConfiguration) and don't try to fetch the
+  //    workspace/document settings.
+  // 2. The client supports workspace/configuration request.
+  //    In this case we ask for the document's settings when we need them (if we don't already have them),
+  //    and clear all the document settings when the configuration changes (onDidChangeConfiguration).
+  // The settings can be configured (in the client) on the user level, workspace level or (on theia) folder level,
+  // using fallback logic from the most specific to most general, so we keep it cached on the document level.
+  if (!hasConfigurationCapability) {
+    return;
+  }
+  if (!hasSettingsForDocument(resource)) {
+    const result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "UI5LanguageAssistant",
+    });
+    setSettingsForDocument(resource, result);
+  }
+}
+
+connection.onDidChangeConfiguration((change) => {
+  if (hasConfigurationCapability) {
+    // Reset all cached document settings
+    clearSettings();
+  } else {
+    if (change.settings.UI5LanguageAssistant !== undefined) {
+      setGlobalSettings(change.settings.UI5LanguageAssistant);
+    }
+  }
+  // No further actions are required currently during configuration change. In the future we might want to
+  // re-validate the files.
+});
+
+// Only keep settings for open documents
+documents.onDidClose((e) => {
+  clearDocumentSettings(e.document.uri);
+});
+
+documents.listen(connection);
+
+connection.listen();
+
+function isXMLView(uri: string): boolean {
+  return /(view|fragment)\.xml$/.test(uri);
+}
